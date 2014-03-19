bf5a802f607042b7d6bbb3b0ec0b02a0b591ca41
Chaya Stern <chayastern54@gmail.com>
2014-03-19 14:37:57 -0400

    Basic outline of my modules and functions

---
+++ coordinates2region.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+"""This module was created to demonstrate testing at the 03/2014 NYU 
+software carpentry bootcamp. The function below contains documentation 
+on their use."""
+
+def coordinates2region()
+	"""Takes input and maps to region"""
+	pass
+
+def main():
+	"""this is the main controller for my module."""
+	print 'Hi!'
+
+if __name__=='__main__':
+	main()
